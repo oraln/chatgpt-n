141ff1b030ca9b214192b6a50eebef1c5f1bfe66
oraln <3138823@qq.com>
2023-04-11 10:00:40 +0800

    二进宫

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "玉玄道精灵",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "玉玄道精灵",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
